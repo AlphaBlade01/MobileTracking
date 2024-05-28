f75da43673a472d17863a30b8752da81b9d4d1f8
AlphaBlade01 <ayaanuppal99@gmail.com>
2024-05-28 18:15:21 +0100

    Creating basic map displaying functionality with dummy data

--- MobileTrackerServer/ViewModels/MainViewModel.cs
+++ MobileTrackerServer/ViewModels/MainViewModel.cs
@@ -1,13 +1,37 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MobileTrackerServer.Models;
+using Syncfusion.Maui.Maps;
+using System.Collections.ObjectModel;
 
 namespace MobileTrackerServer.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
+    public Collection<MobileMarker> MapMarkers { get; set; }
+    public MapLatLng Center { get; set; }
+    public MapZoomPanBehavior ZoomPanBehavior { get; set; }
 
+    public MainViewModel() 
+    {
+        MapMarkers = [];
+        ZoomPanBehavior = new MapZoomPanBehavior
+        {
+            MaxZoomLevel = 19,
+            ZoomLevel = 19,
+            EnableZooming = true,
+            EnablePanning = true
+        };
+
+        MobileMarker marker = new()
+        {
+            Latitude = 52.56902,
+            Longitude = -1.82200,
+            IconHeight = 20,
+            IconWidth = 20,
+            Name = "Samsung S24"
+        };
+
+        Center = new MapLatLng(marker.Latitude, marker.Longitude);
+        MapMarkers.Add(marker);
+    }
 }
